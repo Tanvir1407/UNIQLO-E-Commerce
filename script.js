ad92b7763801b385973a91ecf836e96b5f7dd659
MD. Tanvir <mt14050@gmail.com>
2023-04-25 06:26:28 +0600

    product list design

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ ToggleNav.addEventListener("click", function () {
         ToggleNav.classList.replace("fa-xmark","fa-bars")
     }
 })
+
+
+function filterProduct(e) {
+  const products = document.querySelectorAll(".list > div");
+  const Allbtn = document.querySelectorAll(".Btn");
+  Allbtn.forEach(btn => {
+    if (btn.classList.contains('active')) {
+      btn.classList.remove('active')
+    }
+  })
+  // console.log(e.target.dataset.filter)
+  e.target.classList.add('active')
+  let filter = e.target.dataset.filter;
+  
+  if (filter === "all") {
+    products.forEach(product => {
+      product.classList.remove('hidden')
+    });
+  } else {
+    products.forEach(product => {
+      product.classList.contains(filter) // does the animal have the filter in its class list?
+        ? product.classList.remove('hidden') // if yes, make sure .hidden is not applied
+        : product.classList.add('hidden'); // if no, apply .hidden
+    });
+  }   
+};
+
